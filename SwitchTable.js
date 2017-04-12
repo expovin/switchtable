06a0742ca5f8d0c2533ad780af03ea5272594779
Vincenzo Esposito <expovin@gmail.com>
2017-04-12 13:45:35 +0200

    Aggiunto inner Bootstrap

--- SwitchTable.js
+++ SwitchTable.js
@@ -1,7 +1,8 @@
 define( [
 	"qlik",
 	"jquery", 
-	"text!./style.css", 
+//	"css!./css/style.css", 
+//	"css!./css/scoped-bootstrap.css", 
 	"text!./template.html",
 	"./lib/Settings",
 	"./lib/Controller"
@@ -10,9 +11,10 @@ define( [
 
 		'use strict';
     $("<style>").html(cssContent).appendTo("head");
-	$( '<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">' ).appendTo( 'head' ); // Font Awesome CDN		
-	$( '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">' ).appendTo( 'head' ); // Font Awesome CDN		
-	$( '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>' ).appendTo( 'body' ); // Bootstrap.js CDN
+
+//	$( '<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">' ).appendTo( 'head' ); // Font Awesome CDN		
+//	$( '<link rel="stylesheet" href="./css/scoped-bootstrap.css">' ).appendTo( 'head' ); // Font Awesome CDN		
+//	$( '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>' ).appendTo( 'body' ); // Bootstrap.js CDN
 
 
 	var me = {};
@@ -34,21 +36,16 @@ define( [
 	};
 	me.support = VarSupport;
 
-	me.paint= function ($element,layout) {
+	me.paint= function (layout) {
 			//setup scope.table
 
 			if(!this.$scope.layout.settings) {
 				this.$scope.layout = qlik.layout(this);
 			} 
 
-			/*
-			if ( !this.$scope.table ) {
-				this.$scope.table = qlik.table( this );
-			}
-			*/
-
 			return qlik.Promise.resolve();
 		};
+
 	me.controller = Controller;
 
 	return(me);
