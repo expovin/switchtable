32d04a5d2bc69b49f226544f57de5ce2df07e79e
Vincenzo Esposito <expovin@gmail.com>
2017-04-14 09:48:17 +0200

    Presentazione IT
    Versione Presentata il 13 Aprile all'IT e Almaviva. Release con Eventi
    sul record Treno doversi tra semaforo e motore. Visualizzazione Record
    Eventi per treno su righe (Eventi errati)

--- SwitchTable.js
+++ SwitchTable.js
@@ -14,9 +14,9 @@ define( [
     $("<style>").html(cssContent).appendTo("head");
 
 
-//	$( '<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">' ).appendTo( 'head' ); // Font Awesome CDN		
+	$( '<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">' ).appendTo( 'head' ); // Font Awesome CDN		
 	$( '<link rel="stylesheet" href="/extensions/switchtable/css/scoped-bootstrap.css">' ).appendTo( 'head' ); // Font Awesome CDN		
-//	$( '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>' ).appendTo( 'body' ); // Bootstrap.js CDN
+	$( '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>' ).appendTo( 'body' ); // Bootstrap.js CDN
 
 
 
